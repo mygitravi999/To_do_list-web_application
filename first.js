d301592141d12d1881bad0eaf13603573544eca9
Raviranjan Kumar <ravi2001cpr@gmail.com>
2025-11-05 19:24:24 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,29 @@
+const inputBox = document.getElementById('inputBox');
+const tasklist = document.getElementById('taskList');
+
+function addTask(){
+    const inputText= inputBox.value.trim();
+
+    if(inputText !==''){
+        const li = document.createElement('li');
+        li.innerText = inputText;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText="Remove";
+        deleteButton.addEventListener('click' ,removeTask);
+
+        li.appendChild(deleteButton);
+        tasklist.appendChild(li);
+        inputBox.value='';
+
+    }
+}
+function removeTask(){
+    const listItem =this.parentNode
+    listItem.remove();
+}
+inputBox.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  });
